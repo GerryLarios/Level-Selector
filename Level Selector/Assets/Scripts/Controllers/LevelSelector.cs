6a940c9bff75124612573c0b5683d09316b3aa84
GerryLarios <gerardo.larios69@gmail.com>
2017-09-26 08:37:13 -0500

    Level Selector Unity Project
    This project does not have the fonts or the "Scene fader", its a simpler version but works in the same way.
    
    Watch the video : https://goo.gl/3Nkk9o

---
+++ Level Selector/Assets/Scripts/Controllers/LevelSelector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour {
+
+    public void Select(string levelName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+    }
+}
